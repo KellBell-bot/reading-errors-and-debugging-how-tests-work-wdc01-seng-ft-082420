8ac0d3454998d1d14b1b880bd1c76bd266c06812
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-06 20:49:17 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,10 @@
 first_number = 4
 second_number = 8
 sum = first_number + second_number
-p Sum is #{sum}
+p "Sum is #{sum}"
 difference = first_number - second_number
-p Difference is #{difference}
+p "Difference is #{difference}"
 product = first_number * second_number
-p Product is #{product}
+p "Product is #{product}"
 quotient = first_number / second_number
-p Quotient is #{quotient}
+p "Quotient is #{quotient}"
