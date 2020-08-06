bf595dde7fe95c0e00dad6cc31108cf1a36ed122
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-06 20:50:16 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,10 @@
-# Add your variables here
+first_number = 8
+second_number = 4
+sum = first_number + second_number
+p "Sum is #{sum}"
+difference = first_number - second_number
+p "Difference is #{difference}"
+product = first_number * second_number
+p "Product is #{product}"
+quotient = first_number / second_number
+p "Quotient is #{quotient}"
