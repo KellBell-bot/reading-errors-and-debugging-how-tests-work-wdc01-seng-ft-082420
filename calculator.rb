6ccd80aa91bf89b0d3fc72ec37f941c24f9c0edd
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-06 20:44:03 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,7 @@ sum = first_number + second_number
 p "#{sum}"
 difference = first_number - second_number
 p "#{difference}"
+product = first_number * second_number
+p "#{product}"
+quotient = first_number/second_number
+p "#{quotient}"
